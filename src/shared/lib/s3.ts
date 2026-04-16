03c7e701d4f1a5bb3de4ef2976b81e7822ce09c2
Niko <ivan.nikolaenko@spitch-dev.ai>
2026-04-16 09:35:31 +0300

    fix

--- src/shared/lib/s3.ts
+++ src/shared/lib/s3.ts
@@ -1,11 +1,9 @@
 import * as Minio from 'minio';
 
-const isDevMode = process.env.NODE_ENV === 'development';
-
 export const s3Client = new Minio.Client({
-  endPoint: process.env.S3_ENDPOINT || '',
+  endPoint: process.env.S3_ENDPOINT || 'minio',
   port: process.env.S3_PORT ? Number(process.env.S3_PORT) : undefined,
   accessKey: process.env.S3_ACCESS_KEY,
   secretKey: process.env.S3_SECRET_KEY,
-  useSSL: isDevMode ? false : process.env.S3_USE_SSL === 'true' || true
+  useSSL: false
 });
